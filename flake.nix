7a1d4641230bb5406328c4208f2df10c94795b97
sparky3387 <sparky3387@users.noreply.github.com>
2026-05-27 20:08:59 +1000

    Refactor flake.nix for SABnzbd service configuration

--- flake.nix
+++ flake.nix
@@ -1,72 +1,119 @@
 {
-  description = "SABnzbd service image";
+  description = "minimalbase-ng + sabnzbd service";
 
   inputs = {
-    minimalbase-ng = {
-      url = "github:nonrootdocker/minimalbase-ng";
-    };
-
-    sabnzbd = {
-      url = "github:sabnzbd/sabnzbd";
-      flake = false;
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, minimalbase-ng, sabnzbd }:
+  outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
 
-    pkgs = minimalbase-ng.packages.${system}.pkgs;
+    # ----------------------------
+    # SABnzbd package
+    # ----------------------------
+    sabnzbd = pkgs.stdenv.mkDerivation {
+      pname = "sabnzbd";
+      version = "latest";
 
-    baseImage =
-      minimalbase-ng.packages.${system}.base-image;
+      src = ./sabnzbd-src;
+
+      buildInputs = [
+        pkgs.python3
+        pkgs.python3Packages.virtualenv
+        pkgs.python3Packages.pip
+      ];
+
+      installPhase = ''
+        mkdir -p $out/app
+        mkdir -p $out/data
+
+        # Python venv
+        python3 -m venv $out/app/python-venv
+        $out/app/python-venv/bin/pip install --upgrade pip
+
+        if [ -f requirements.txt ]; then
+          $out/app/python-venv/bin/pip install -r requirements.txt
+        fi
+
+        # SABnzbd source
+        cp -r . $out/app/sabnzbd-src
+
+        # REQUIRED ENTRYPOINT
+        if [ -f SABnzbd.py ]; then
+          cp SABnzbd.py $out/app/main.py
+        else
+          echo "ERROR: SABnzbd.py not found"
+          exit 1
+        fi
+      '';
+    };
+
+    # ----------------------------
+    # ABI generator (NO shell)
+    # ----------------------------
+    sabAbi = pkgs.writeText "sabnzbd-abi.json" (builtins.toJSON {
+      version = 2;
+
+      process = {
+        exec = "python";
+        args = [
+          "--config-file"
+          "/data/sabnzbd.ini"
+          "--logging"
+          "0"
+          "--console"
+          "--browser"
+          "0"
+        ];
+      };
+    });
+
+    # ----------------------------
+    # Rust PID1
+    # ----------------------------
+    container-init = pkgs.rustPlatform.buildRustPackage {
+      pname = "container-init";
+      version = "0.2.0";
+
+      src = ./rust-init;
+
+      cargoLock = {
+        lockFile = ./rust-init/Cargo.lock;
+      };
+    };
 
   in {
-    packages.${system}.default =
-      pkgs.dockerTools.buildImage {
-        name = "sabnzbd";
+    packages.${system} = {
+      base-image = pkgs.dockerTools.buildImage {
+        name = "minimalbase-ng";
         tag = "latest";
-
-        fromImage = baseImage;
 
         copyToRoot = pkgs.buildEnv {
           name = "root";
+          paths = [
+            pkgs.coreutils
+            pkgs.tzdata
+            pkgs.cacert
 
-          paths = with pkgs; [
-            coreutils
-            python3
-            python3Packages.pip
-            par2cmdline
-            unrar
-            p7zip
+            container-init
+            sabnzbd
+            sabAbi
           ];
         };
 
-        extraCommands = ''
-          mkdir -p /app
-          cp -r ${sabnzbd} /app/sabnzbd
-          python3 -m venv /app/python-venv
-          /app/python-venv/bin/python3 -m pip install -r /app/sabnzbd/requirements.txt
-        '';
-
         config = {
-          Entrypoint = [ "/bin/container-init" ];
+          Entrypoint = [ "${container-init}/bin/container-init" ];
 
-          Cmd = [
-            "/app/python-venv/bin/python3"
-            "/app/sabnzbd/SABnzbd.py"
-            "--config-file" "/data/sabnzbd.ini"
-            "--logging" "0"
-            "--console"
-            "--browser" "0"
-          ];
+          Cmd = [ sabAbi ];
 
           Env = [
             "TZ=UTC"
             "LANG=en_US.UTF-8"
-            "PYTHONUNBUFFERED=1"
           ];
         };
       };
+    };
   };
 }
